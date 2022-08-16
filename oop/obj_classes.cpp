9512e8819e277c2a1e7a3a335fd82f2ec89209e1
Dembytskyi Pavlo <nekto.nekotov@gmail.com>
2022-08-16 22:05:29 +0200

    mg

---
+++ oop/obj_classes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <assert.h>
+
+class Box{
+
+	public:
+	double lenght,breadth,height;
+	double weight;
+	
+	double volume() {
+		assert(lenght>0);
+		assert(breadth>0);
+		assert(height>0);
+		return lenght*breadth*height;
+	}
+	
+};
+
+int main(){
+
+	Box b1;
+	b1.lenght = 5;
+	b1.breadth=15;
+	b1.height=3;
+
+	std::cout << b1.volume() << "\n";
+}
